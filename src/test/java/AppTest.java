9221b3bc682389fe1ed877d27a663ea40e7f5620
Tassio Lima <tassiolima@Tassios-MBP.lan>
2023-09-14 15:24:53 +0100

    update

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -14,6 +14,7 @@ public class AppTest extends Hooks{
         pageMini.validateStatusInterstitial();
     }
 
+
     @Test
     @DisplayName("Validate change of banner status")
     public void changeStatusBanner() {
